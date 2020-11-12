3e49fc1b9c7a60e654587c2a248eba4fa6314130
adsalomo <adsalomo@gmail.com>
2020-11-11 20:07:59 -0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   urlApiFirebase: 'https://identitytoolkit.googleapis.com/v1/accounts:',
   urlApiMovie: '',
-  keyAuthFirebase: 'AIzaSyBH3_YyVbIpfnyRcqod6Vuu7auGP4ShVqo'
+  keyAuthFirebase: ''
 };
