d75921d5bddf821a42e2b3d024fa10e191655d26
adsalomo <adsalomo@gmail.com>
2020-11-18 18:38:30 -0500

    feat: ajuste cerrar sesión

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +11,30 @@ export class HeaderComponent implements OnInit {
 
   collapsed = true;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   toggleCollapsed(): void {
     this.collapsed = !this.collapsed;
+  }
+
+  async logout() {
+    const resp = await Swal.fire({
+      title: 'Cerrar sesión',
+      icon: 'info',
+      text: '¿Está seguro de cerrar su sesión?',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'OK'
+    });
+
+    if (resp.value) {
+      localStorage.removeItem('token');
+      this.router.navigateByUrl('login');
+    }
   }
 
 }
